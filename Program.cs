e77072b2a5b7b46a6e0035cbe9a5af7f79eabdae
Maxim Sidorenko <Maxim Sidorenko@DESKTOP-IIFDPU3>
2025-03-22 20:44:40 +0300

    INSERT DONE

--- Program.cs
+++ Program.cs
@@ -21,58 +21,8 @@ namespace ADO.NET
 			Connector.InsertDirector("George", "Martin");
 			Connector.SelectDirectors();
 			Connector.SelectMovies();
-
-			/*	const int PADDING = 30;
-				try
-				{
-					const string CONNECTION_STRING = 
-						"Data Source=(localdb)\\MSSQLLocalDB;" +
-						"Initial Catalog=Movies;Integrated Security=True;" +
-						"Connect Timeout=30;Encrypt=False;" +
-						"TrustServerCertificate=False;" +
-						"ApplicationIntent=ReadWrite;" +
-						"MultiSubnetFailover=False";
-					Console.WriteLine(CONNECTION_STRING);
-
-					SqlConnection connection = new SqlConnection(CONNECTION_STRING);
-
-					SqlCommand command = new SqlCommand(cmd, connection);
-
-					connection.Open();
-
-					SqlDataReader reader = command.ExecuteReader();
-
-
-					if (reader.HasRows)
-					{
-
-						Console.WriteLine("====================================================================");
-						for(int i = 0; i < reader.FieldCount; i++)
-						{
-							Console.Write(reader.GetName(i).PadRight(PADDING));
-						}
-						Console.WriteLine();
-						Console.WriteLine("====================================================================");
-						while (reader.Read())
-						{
-							//Console.WriteLine(reader[0].ToString().PadRight(5) + reader[2].ToString().PadRight(15) + reader[1].ToString());
-							for (int i = 0; i < reader.FieldCount; i++)
-							{
-								Console.Write(reader[i].ToString().PadRight(PADDING));
-							}
-							Console.WriteLine();
-						}
-					}
-					reader.Close();
-					connection.Dispose();
-					connection.Close();
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e.Message);
-				}*/
-
-
+		
+		
 		}
 	}
 }
