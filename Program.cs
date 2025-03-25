d2ef3a047d8a5db3709b244df03be6bcef2c3173
Maxim Sidorenko <Maxim Sidorenko@DESKTOP-IIFDPU3>
2025-03-25 13:27:07 +0300

    HomeWork DONE

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
+using System.Management;
 using System.Data.SqlClient;
 
 namespace ADO.NET
@@ -16,15 +17,16 @@ namespace ADO.NET
 			const int PADDING = 30;
 			try
 			{
-				const string CONNECTION_STRING = 
-					"Data Source=(localdb)\\MSSQLLocalDB;" +
-					"Initial Catalog=Movies;Integrated Security=True;" +
-					"Connect Timeout=30;Encrypt=False;" +
-					"TrustServerCertificate=False;" +
-					"ApplicationIntent=ReadWrite;" +
-					"MultiSubnetFailover=False";
+				//const string CONNECTION_STRING = 
+				//	"Data Source=(localdb)\\MSSQLLocalDB;" +
+				//	"Initial Catalog=Movies;Integrated Security=True;" +
+				//	"Connect Timeout=30;Encrypt=False;" +
+				//	"TrustServerCertificate=False;" +
+				//	"ApplicationIntent=ReadWrite;" +
+				//	"MultiSubnetFailover=False";
+				/*static readonly*/
+				string CONNECTION_STRING = ConfigurationManager.ConnectionStrings["Movies"].ConnectionString;
 				Console.WriteLine(CONNECTION_STRING);
-				
 				SqlConnection connection = new SqlConnection(CONNECTION_STRING);
 
 				string cmd = "SELECT title,release_date,FORMATMESSAGE(N'%s %s', first_name, last_name) AS director FROM Movies, Directors WHERE director=director_id";
@@ -33,13 +35,13 @@ namespace ADO.NET
 				connection.Open();
 
 				SqlDataReader reader = command.ExecuteReader();
-				
+
 
 				if (reader.HasRows)
 				{
 
 					Console.WriteLine("====================================================================");
-					for(int i = 0; i < reader.FieldCount; i++)
+					for (int i = 0; i < reader.FieldCount; i++)
 					{
 						Console.Write(reader.GetName(i).PadRight(PADDING));
 					}
