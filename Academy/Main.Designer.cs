f573afbaf891e329fbb3129232669ce8b6c9cf15
Maxim Sidorenko <Maxim Sidorenko@DESKTOP-IIFDPU3>
2025-03-29 07:00:59 +0300

    Filters in progress

--- Academy/Main.Designer.cs
+++ Academy/Main.Designer.cs
@@ -28,12 +28,19 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.statusStrip = new System.Windows.Forms.StatusStrip();
 			this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
 			this.tabControl = new System.Windows.Forms.TabControl();
 			this.tpStudent = new System.Windows.Forms.TabPage();
+			this.cbStudentsGroups = new System.Windows.Forms.ComboBox();
+			this.cbDirectionsStudents = new System.Windows.Forms.ComboBox();
 			this.dgvStudents = new System.Windows.Forms.DataGridView();
+			this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.cmRefreshTable = new System.Windows.Forms.ToolStripMenuItem();
+			this.cmSeeAllDirctions = new System.Windows.Forms.ToolStripMenuItem();
 			this.tbGroups = new System.Windows.Forms.TabPage();
+			this.cbDirectionsGroups = new System.Windows.Forms.ComboBox();
 			this.dgvGroups = new System.Windows.Forms.DataGridView();
 			this.tpDirections = new System.Windows.Forms.TabPage();
 			this.dgvDirections = new System.Windows.Forms.DataGridView();
@@ -41,11 +48,12 @@
 			this.dgvDisciplines = new System.Windows.Forms.DataGridView();
 			this.tbTeahcers = new System.Windows.Forms.TabPage();
 			this.dgvTeachers = new System.Windows.Forms.DataGridView();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.toolStripFilterCount = new System.Windows.Forms.ToolStripStatusLabel();
 			this.statusStrip.SuspendLayout();
 			this.tabControl.SuspendLayout();
 			this.tpStudent.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+			this.contextMenu.SuspendLayout();
 			this.tbGroups.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dgvGroups)).BeginInit();
 			this.tpDirections.SuspendLayout();
@@ -58,8 +66,10 @@
 			// 
 			// statusStrip
 			// 
+			this.statusStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
 			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel});
+            this.toolStripStatusLabel,
+            this.toolStripFilterCount});
 			this.statusStrip.Location = new System.Drawing.Point(0, 419);
 			this.statusStrip.Name = "statusStrip";
 			this.statusStrip.Size = new System.Drawing.Size(624, 22);
@@ -68,6 +78,7 @@
 			// 
 			// toolStripStatusLabel
 			// 
+			this.toolStripStatusLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
 			this.toolStripStatusLabel.Name = "toolStripStatusLabel";
 			this.toolStripStatusLabel.Size = new System.Drawing.Size(112, 17);
 			this.toolStripStatusLabel.Text = "toolStripStatusLabel";
@@ -86,11 +97,13 @@
 			this.tabControl.Size = new System.Drawing.Size(624, 419);
 			this.tabControl.TabIndex = 1;
 			this.tabControl.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged);
+			this.tabControl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tabControl_MouseClick);
 			// 
 			// tpStudent
 			// 
-			this.tpStudent.BackColor = System.Drawing.Color.Transparent;
-			this.tpStudent.Controls.Add(this.comboBox1);
+			this.tpStudent.BackColor = System.Drawing.Color.Teal;
+			this.tpStudent.Controls.Add(this.cbStudentsGroups);
+			this.tpStudent.Controls.Add(this.cbDirectionsStudents);
 			this.tpStudent.Controls.Add(this.dgvStudents);
 			this.tpStudent.Location = new System.Drawing.Point(4, 22);
 			this.tpStudent.Name = "tpStudent";
@@ -99,20 +112,69 @@
 			this.tpStudent.TabIndex = 0;
 			this.tpStudent.Text = "Students";
 			// 
+			// cbStudentsGroups
+			// 
+			this.cbStudentsGroups.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbStudentsGroups.FormattingEnabled = true;
+			this.cbStudentsGroups.Location = new System.Drawing.Point(304, 15);
+			this.cbStudentsGroups.Name = "cbStudentsGroups";
+			this.cbStudentsGroups.Size = new System.Drawing.Size(304, 21);
+			this.cbStudentsGroups.TabIndex = 2;
+			this.cbStudentsGroups.SelectedIndexChanged += new System.EventHandler(this.cbStudents_SelectedIndexChanged);
+			// 
+			// cbDirectionsStudents
+			// 
+			this.cbDirectionsStudents.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbDirectionsStudents.FormattingEnabled = true;
+			this.cbDirectionsStudents.Location = new System.Drawing.Point(8, 15);
+			this.cbDirectionsStudents.Name = "cbDirectionsStudents";
+			this.cbDirectionsStudents.Size = new System.Drawing.Size(290, 21);
+			this.cbDirectionsStudents.TabIndex = 1;
+			this.cbDirectionsStudents.DropDown += new System.EventHandler(this.cbDirectionsStudents_DropDown);
+			this.cbDirectionsStudents.SelectedIndexChanged += new System.EventHandler(this.cbDirections_SelectedIndexChanged);
+			// 
 			// dgvStudents
 			// 
 			this.dgvStudents.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dgvStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvStudents.BackgroundColor = System.Drawing.Color.CadetBlue;
 			this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvStudents.ContextMenuStrip = this.contextMenu;
 			this.dgvStudents.Location = new System.Drawing.Point(3, 42);
 			this.dgvStudents.Name = "dgvStudents";
 			this.dgvStudents.Size = new System.Drawing.Size(610, 348);
 			this.dgvStudents.TabIndex = 0;
 			// 
+			// contextMenu
+			// 
+			this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmRefreshTable,
+            this.cmSeeAllDirctions});
+			this.contextMenu.Name = "contextMenu";
+			this.contextMenu.Size = new System.Drawing.Size(164, 48);
+			// 
+			// cmRefreshTable
+			// 
+			this.cmRefreshTable.Name = "cmRefreshTable";
+			this.cmRefreshTable.Size = new System.Drawing.Size(163, 22);
+			this.cmRefreshTable.Text = "Refresh Table";
+			this.cmRefreshTable.Click += new System.EventHandler(this.cmRefreshTable_Click);
+			// 
+			// cmSeeAllDirctions
+			// 
+			this.cmSeeAllDirctions.Name = "cmSeeAllDirctions";
+			this.cmSeeAllDirctions.Size = new System.Drawing.Size(163, 22);
+			this.cmSeeAllDirctions.Text = "See all Directions";
+			this.cmSeeAllDirctions.CheckedChanged += new System.EventHandler(this.cmSeeAllDirctions_CheckedChanged);
+			this.cmSeeAllDirctions.Click += new System.EventHandler(this.cmSeeAllDirctions_Click);
+			// 
 			// tbGroups
 			// 
+			this.tbGroups.BackColor = System.Drawing.Color.Teal;
+			this.tbGroups.Controls.Add(this.cbDirectionsGroups);
 			this.tbGroups.Controls.Add(this.dgvGroups);
 			this.tbGroups.Location = new System.Drawing.Point(4, 22);
 			this.tbGroups.Name = "tbGroups";
@@ -120,7 +182,18 @@
 			this.tbGroups.Size = new System.Drawing.Size(616, 393);
 			this.tbGroups.TabIndex = 1;
 			this.tbGroups.Text = "Groups";
-			this.tbGroups.UseVisualStyleBackColor = true;
+			// 
+			// cbDirectionsGroups
+			// 
+			this.cbDirectionsGroups.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbDirectionsGroups.FormattingEnabled = true;
+			this.cbDirectionsGroups.Location = new System.Drawing.Point(8, 15);
+			this.cbDirectionsGroups.Name = "cbDirectionsGroups";
+			this.cbDirectionsGroups.Size = new System.Drawing.Size(600, 21);
+			this.cbDirectionsGroups.TabIndex = 1;
+			this.cbDirectionsGroups.DropDown += new System.EventHandler(this.cbDirectionsGroups_DropDown);
+			this.cbDirectionsGroups.SelectedIndexChanged += new System.EventHandler(this.cbDirectionsGroups_SelectedIndexChanged);
 			// 
 			// dgvGroups
 			// 
@@ -128,7 +201,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dgvGroups.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvGroups.BackgroundColor = System.Drawing.Color.CadetBlue;
 			this.dgvGroups.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvGroups.ContextMenuStrip = this.contextMenu;
 			this.dgvGroups.Location = new System.Drawing.Point(3, 42);
 			this.dgvGroups.Name = "dgvGroups";
 			this.dgvGroups.Size = new System.Drawing.Size(610, 348);
@@ -136,6 +211,7 @@
 			// 
 			// tpDirections
 			// 
+			this.tpDirections.BackColor = System.Drawing.Color.Teal;
 			this.tpDirections.Controls.Add(this.dgvDirections);
 			this.tpDirections.Location = new System.Drawing.Point(4, 22);
 			this.tpDirections.Name = "tpDirections";
@@ -143,7 +219,6 @@
 			this.tpDirections.Size = new System.Drawing.Size(616, 393);
 			this.tpDirections.TabIndex = 2;
 			this.tpDirections.Text = "Directions";
-			this.tpDirections.UseVisualStyleBackColor = true;
 			// 
 			// dgvDirections
 			// 
@@ -151,7 +226,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dgvDirections.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvDirections.BackgroundColor = System.Drawing.Color.CadetBlue;
 			this.dgvDirections.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvDirections.ContextMenuStrip = this.contextMenu;
 			this.dgvDirections.Location = new System.Drawing.Point(3, 42);
 			this.dgvDirections.Name = "dgvDirections";
 			this.dgvDirections.Size = new System.Drawing.Size(610, 348);
@@ -159,6 +236,7 @@
 			// 
 			// tpDisciplines
 			// 
+			this.tpDisciplines.BackColor = System.Drawing.Color.Teal;
 			this.tpDisciplines.Controls.Add(this.dgvDisciplines);
 			this.tpDisciplines.Location = new System.Drawing.Point(4, 22);
 			this.tpDisciplines.Name = "tpDisciplines";
@@ -166,7 +244,6 @@
 			this.tpDisciplines.Size = new System.Drawing.Size(616, 393);
 			this.tpDisciplines.TabIndex = 3;
 			this.tpDisciplines.Text = "Disciplines";
-			this.tpDisciplines.UseVisualStyleBackColor = true;
 			// 
 			// dgvDisciplines
 			// 
@@ -174,7 +251,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dgvDisciplines.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvDisciplines.BackgroundColor = System.Drawing.Color.CadetBlue;
 			this.dgvDisciplines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvDisciplines.ContextMenuStrip = this.contextMenu;
 			this.dgvDisciplines.Location = new System.Drawing.Point(3, 42);
 			this.dgvDisciplines.Name = "dgvDisciplines";
 			this.dgvDisciplines.Size = new System.Drawing.Size(610, 348);
@@ -182,6 +261,7 @@
 			// 
 			// tbTeahcers
 			// 
+			this.tbTeahcers.BackColor = System.Drawing.Color.Teal;
 			this.tbTeahcers.Controls.Add(this.dgvTeachers);
 			this.tbTeahcers.Location = new System.Drawing.Point(4, 22);
 			this.tbTeahcers.Name = "tbTeahcers";
@@ -189,7 +269,6 @@
 			this.tbTeahcers.Size = new System.Drawing.Size(616, 393);
 			this.tbTeahcers.TabIndex = 4;
 			this.tbTeahcers.Text = "Teachers";
-			this.tbTeahcers.UseVisualStyleBackColor = true;
 			// 
 			// dgvTeachers
 			// 
@@ -197,24 +276,26 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dgvTeachers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvTeachers.BackgroundColor = System.Drawing.Color.CadetBlue;
 			this.dgvTeachers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvTeachers.ContextMenuStrip = this.contextMenu;
 			this.dgvTeachers.Location = new System.Drawing.Point(3, 42);
 			this.dgvTeachers.Name = "dgvTeachers";
 			this.dgvTeachers.Size = new System.Drawing.Size(610, 348);
 			this.dgvTeachers.TabIndex = 0;
 			// 
-			// comboBox1
+			// toolStripFilterCount
 			// 
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(8, 15);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(312, 21);
-			this.comboBox1.TabIndex = 1;
+			this.toolStripFilterCount.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.toolStripFilterCount.Name = "toolStripFilterCount";
+			this.toolStripFilterCount.Size = new System.Drawing.Size(118, 17);
+			this.toolStripFilterCount.Text = "toolStripStatusLabel1";
 			// 
 			// Main
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Teal;
 			this.ClientSize = new System.Drawing.Size(624, 441);
 			this.Controls.Add(this.tabControl);
 			this.Controls.Add(this.statusStrip);
@@ -227,6 +308,7 @@
 			this.tabControl.ResumeLayout(false);
 			this.tpStudent.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+			this.contextMenu.ResumeLayout(false);
 			this.tbGroups.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.dgvGroups)).EndInit();
 			this.tpDirections.ResumeLayout(false);
@@ -255,7 +337,13 @@
 		private System.Windows.Forms.DataGridView dgvDisciplines;
 		private System.Windows.Forms.DataGridView dgvTeachers;
 		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
-		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox cbDirectionsStudents;
+		private System.Windows.Forms.ComboBox cbStudentsGroups;
+		private System.Windows.Forms.ContextMenuStrip contextMenu;
+		private System.Windows.Forms.ToolStripMenuItem cmRefreshTable;
+		private System.Windows.Forms.ComboBox cbDirectionsGroups;
+		private System.Windows.Forms.ToolStripMenuItem cmSeeAllDirctions;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripFilterCount;
 	}
 }
 
--- Academy/Main.cs
+++ Academy/Main.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,51 +20,205 @@ namespace Academy
 			connector = new Connector(ConfigurationManager.ConnectionStrings["PV_319_IMPORT"].ConnectionString);
 			InitializeComponent();
 			Filling();
+
+
 		}
 		public void Filling()
 		{
 			toolStripStatusLabel.Text = "Количество студентов: " + connector.Count("Students").ToString();
-			
+
 			dgvStudents.DataSource = connector.Select("FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS N'Студент', birth_date AS N'Дата Рождения', group_name AS N'Группа', direction_name AS N'Направление'", "Students, Groups, Directions", "[group]=group_id AND direction=direction_id");
-			
+			cmSeeAllDirctions.Visible = false;
+			toolStripFilterCount.Text = "";
+
 			//dgvGroups.DataSource = connector.Select("group_name AS N'Группа', direction_name AS N'Направление', start_time AS N'Время начала занятий'", "Groups, Directions", "direction=direction_id");
-			
 			//dgvDirections.DataSource = connector.Select("direction_name AS N'Направление'", "Directions");
-			
 			//dgvDisciplines.DataSource = connector.Select("discipline_name AS N'Название дисциплины', number_of_lessons AS N'Количество занятий'", "Disciplines");
-			
 			//dgvTeachers.DataSource = connector.Select("FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS 'Преподователь', birth_date AS N'Дата рождения', work_since AS N'Опыт работы', rate AS N'Ставка'", "Teachers");
-			
-			//dgvStudents.DataSource = connector.Select("*", "Students");
-			//dgvGroups.DataSource = connector.Select("*", "Groups");
-			//dgvDirections.DataSource = connector.Select("*", "Directions");
-			//dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
-			//dgvTeachers.DataSource = connector.Select("*", "Teachers");
+			//Console.WriteLine(dgvDirections.Columns[0]);
+
 		}
 
-		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+		public void RefreshTable(bool isNeedResetCB = false)
 		{
-			switch ((sender as TabControl).SelectedIndex)
+			switch (tabControl.SelectedIndex)
 			{
 				case 0:
-					toolStripStatusLabel.Text = "Количество студентов: " + connector.Count("Students");
+					{
+						dgvStudents.DataSource = connector.Select("FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS N'Студент', birth_date AS N'Дата Рождения', group_name AS N'Группа', direction_name AS N'Направление'", "Students, Groups, Directions", "[group]=group_id AND direction=direction_id");
+						toolStripStatusLabel.Text = "Количество студентов: " + connector.Count("Students");
+						if (isNeedResetCB)
+						{
+							cbDirectionsStudents.Text = "";
+							cbStudentsGroups.Text = "";
+							cbStudentsGroups.Items.Clear();
+						}
+						cmSeeAllDirctions.Visible = false;
+						toolStripFilterCount.Text = "";
+					}
 					break;
 				case 1:
-					toolStripStatusLabel.Text = "Количество групп: " + connector.Count("Groups");
-					dgvGroups.DataSource = connector.Select("group_name AS N'Группа', direction_name AS N'Направление', start_time AS N'Время начала занятий'", "Groups, Directions", "direction=direction_id");
+					{
+						toolStripStatusLabel.Text = "Количество групп: " + connector.Count("Groups");
+						dgvGroups.DataSource = connector.Select("group_name AS N'Группа', direction_name AS N'Направление', dbo.GetLearningDays(group_name) AS N'Дни недели', start_time AS N'Время начала занятий'", "Groups, Directions", "direction=direction_id");
+						if (isNeedResetCB)
+						{
+							cbDirectionsGroups.Text = "";
+						}
+						cmSeeAllDirctions.Visible = false;
+						toolStripFilterCount.Text = "";
+					}
 					break;
 				case 2:
-					toolStripStatusLabel.Text = "Количество направлений: " + connector.Count("Directions");
-					dgvDirections.DataSource = connector.Select("direction_name AS N'Направление'", "Directions");
+					toolStripStatusLabel.Text = "Количество всех направлений: " + connector.Count("Directions");
+					cmSeeAllDirctions.Visible = true;
+					if (!cmSeeAllDirctions.Checked)
+					{
+						dgvDirections.DataSource = connector.Select
+																	(
+																		"direction_name AS N'Направление', COUNT(DISTINCT group_id) AS N'Количество групп', COUNT(stud_id) AS N'Количество студентов'",
+																		"Directions, Groups LEFT JOIN Students ON [group]=group_id",
+																		"direction=direction_id GROUP BY direction_name"
+																	);
+						toolStripFilterCount.Visible = true;
+						toolStripFilterCount.Text = "Количество направлений с группами: " + (dgvDirections.Rows.Count - 1);
+					}
 					break;
 				case 3:
 					toolStripStatusLabel.Text = "Количество дисциплин: " + connector.Count("Disciplines");
 					dgvDisciplines.DataSource = connector.Select("discipline_name AS N'Название дисциплины', number_of_lessons AS N'Количество занятий'", "Disciplines");
+					cmSeeAllDirctions.Visible = false;
+					toolStripFilterCount.Text = "";
 					break;
 				case 4:
 					toolStripStatusLabel.Text = "Количество преподователей: " + connector.Count("Teachers");
 					dgvTeachers.DataSource = connector.Select("FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS 'Преподователь', birth_date AS N'Дата рождения', work_since AS N'Опыт работы', rate AS N'Ставка'", "Teachers");
+					cmSeeAllDirctions.Visible = false;
+					toolStripFilterCount.Text = "";
 					break;
+			}
+		}
+
+		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			RefreshTable();
+		}
+
+		private void cbDirections_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			cbStudentsGroups.Items.Clear();
+			if (cbDirectionsStudents.SelectedIndex != -1)
+			{
+				dgvStudents.DataSource = connector.Select(
+					"FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS N'Студент', birth_date AS N'Дата Рождения', group_name AS N'Группа', direction_name AS N'Направление'",
+					"Students, Groups, Directions",
+					$"[group]=group_id AND direction=direction_id AND direction_name=N'{cbDirectionsStudents.SelectedItem}'");
+				for (int i = 0; i < dgvStudents.RowCount - 1; i++)
+				{
+					if (cbStudentsGroups.Items.Contains(dgvStudents.Rows[i].Cells["Группа"].Value))
+					{
+						continue;
+					}
+					cbStudentsGroups.Items.Add(dgvStudents.Rows[i].Cells["Группа"].Value.ToString());
+				}
+			}
+		}
+
+		private void cbStudents_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cbStudentsGroups.SelectedIndex != -1)
+			{
+				dgvStudents.DataSource = connector.Select(
+				"FORMATMESSAGE(N'%s %s %s', last_name, first_name, middle_name) AS N'Студент', birth_date AS N'Дата Рождения', group_name AS N'Группа', direction_name AS N'Направление'",
+				"Students, Groups, Directions",
+				$"[group]=group_id AND direction=direction_id AND direction_name=N'{cbDirectionsStudents.SelectedItem}' AND group_name=N'{cbStudentsGroups.SelectedItem}'");
+			}
+			if (cbStudentsGroups.SelectedIndex == -1)
+			{
+				cbStudentsGroups.Items.Clear();
+			}
+		}
+
+		private void tabControl_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (e.Button.HasFlag(MouseButtons.Right))
+			{
+				contextMenu.Show(Cursor.Position);
+			}
+		}
+
+		private void cmRefreshTable_Click(object sender, EventArgs e)
+		{
+			RefreshTable(true);
+		}
+
+
+		private void cbDirectionsStudents_DropDown(object sender, EventArgs e)
+		{
+
+			if (dgvDirections.Rows.Count == 0 || cbDirectionsStudents.Items.Count == 0)
+			{
+
+				this.SuspendLayout();
+				tabControl.Hide();
+
+				tabControl.SelectTab("tpDirections");
+				for (int i = 0; i < dgvDirections.RowCount - 1; i++)
+				{
+					cbDirectionsStudents.Items.Add(dgvDirections.Rows[i].Cells[0].Value.ToString());
+				}
+				tabControl.SelectTab("tpStudent");
+				tabControl.Show();
+				this.ResumeLayout();
+			}
+		}
+
+		private void cbDirectionsGroups_DropDown(object sender, EventArgs e)
+		{
+			if (dgvDirections.Rows.Count == 0 || cbDirectionsGroups.Items.Count == 0)
+			{
+				this.SuspendLayout();
+				tabControl.Hide();
+
+				tabControl.SelectTab("tpDirections");
+				for (int i = 0; i < dgvDirections.Rows.Count - 1; i++)
+				{
+					cbDirectionsGroups.Items.Add(dgvDirections.Rows[i].Cells[0].Value.ToString());
+				}
+				tabControl.SelectTab("tbGroups");
+				tabControl.Show();
+				this.ResumeLayout();
+			}
+		}
+
+		private void cbDirectionsGroups_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cbDirectionsGroups.SelectedIndex != -1)
+			{
+				dgvGroups.DataSource = connector.Select
+					(
+					"group_name AS N'Группа', direction_name AS N'Направление', start_time AS N'Время начала занятий'", "Groups, Directions", $"direction=direction_id AND direction_name=N'{cbDirectionsGroups.SelectedItem}'"
+					);
+			}
+		}
+
+		private void cmSeeAllDirctions_Click(object sender, EventArgs e)
+		{
+			cmSeeAllDirctions.Checked = cmSeeAllDirctions.Checked ? false : true;
+		}
+
+		private void cmSeeAllDirctions_CheckedChanged(object sender, EventArgs e)
+		{
+			cbDirectionsGroups.Items.Clear();
+			cbDirectionsStudents.Items.Clear();
+			if (cmSeeAllDirctions.Checked)
+			{
+				toolStripFilterCount.Text = "";
+				dgvDirections.DataSource = connector.Select("direction_name AS N'Направление'", "Directions");
+			}
+			if (!cmSeeAllDirctions.Checked)
+			{
+				RefreshTable();
 			}
 		}
 	}
